15993a4e03c9db7f0be71c023af3a8686121172e
juniorDev <sotundeemmanuel@gmail.com>
2023-11-05 11:01:25 +0100

    updated work

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,19 @@
 "use strict";
 
-// storing elements in variable
+// # STORING ELEMENTS IN A VARIABLE
 
-// navigation bar elements
+// ## NAVIGATION BAR ELEMENTTS
 const btnOpenNav = document.querySelector(".btn-opennav");
 const btnCloseNav = document.querySelector(".btn-closenav");
 
 const navigationBar = document.querySelector(".menu_navbar");
 
-// cart elements
+// ## CART ELEMENTS
 const cartEL = document.querySelector(".cart");
 const whiteCartEL = document.querySelector(".white-cart");
 const colorCartEL = document.querySelector(".color-cart");
 
-// bestseller elements
+// ## BESTSELLER ELEMENTS
 const booksEL = document.querySelectorAll(".book");
 const bookImageEL = document.querySelectorAll(".book-image");
 const shoppingInfoEL = document.querySelectorAll(".shopping-info");
@@ -21,7 +21,7 @@ const bookTitleEL = document.querySelectorAll(".book-title");
 const bookAuthorEL = document.querySelectorAll(".book-author");
 const overlayEL = document.querySelectorAll(".overlay");
 
-// destructuring the elements
+// ### DESTRUCTURING THE BESTSELLER ELEMENTS
 const [bookOne, bookTwo, bookThree, bookFour] = booksEL;
 const [bookImageOneEL, bookImageTwoEL, bookImageThreeEL, bookImageFourEL] =
   bookImageEL;
@@ -38,41 +38,66 @@ const [bookAuthorOneEL, bookAuthorTwoEL, bookAuthorThreeEL, bookAuthorFourEL] =
 const [overlayOneEL, overlayTwoEL, overlayThreeEL, overlayFourEL] = overlayEL;
 console.log(overlayOneEL, overlayThreeEL);
 
-// -- BOOKS CATEGORIES ELEMENTS --
+// ## BOOKS CATEGORIES ELEMENTS --
 const bcOne = document.querySelector(".bc-one");
 const bcTwo = document.querySelector(".bc-two");
 const bcThree = document.querySelector(".bc-three");
 const bcFour = document.querySelector(".bc-four");
-const infoEL = docuement.querySelectorAll(".info");
+const infoEL = document.querySelectorAll(".info");
 const bcTitle = document.querySelectorAll(".bc-title");
 const btnSeeAll = document.querySelectorAll(".btn-see-all");
 
-const overlayCTOne = document.querySelector(".ct-one");
+const overlayCtOne = document.querySelector(".ct-one");
+const overlayCtTwo = document.querySelector(".ct-two");
+const overlayCtThree = document.querySelector(".ct-three");
+const overlayCtFour = document.querySelector(".ct-four");
 
+// ### DESTRUCTURING THE CATEGORIES ELEMENTS
 const [infoOne, infoTwo, infoThree, infoFour] = infoEL;
 const [bcTitleOne, bcTitleTwo, bcTitleThree, bcTitleFour] = bcTitle;
 const [btnOne, btnTwo, btnThree, btnFour] = btnSeeAll;
 
-// -- REUSABLE FUNCTIONS ---
+// ## TESTIMONIAL ELEMENT
+const testimonialsEL = document.querySelectorAll(".testimonial");
+const testName = document.querySelectorAll(".test-name");
+
+// ### DESTRUCTURING THE TESTIMONIAL ELEMENTS
+const [testimonialOne, testimonialTwo, testimonialThree] = testimonialsEL;
+const [testNameOne, testNameTwo, testNameThree] = testName;
+console.log(testimonialOne);
+
+// ## SOCIAL MEDIA HANDLES ELEMENTS
+const facebookEL = document.querySelector(".facebook");
+const outlineFacebook = document.querySelector(".outline-facebook");
+const coloredFacebook = document.querySelector(".colored-facebook");
+
+const instagramEL = document.querySelector(".instagram");
+const outlineInstagram = document.querySelector(".outline-instagram");
+const coloredInstagram = document.querySelector(".colored-instagram");
+
+const twitterEL = document.querySelector(".twitter");
+const outlineTwitter = document.querySelector(".outline-twitter");
+const coloredTwitter = document.querySelector(".colored-twitter");
+// -- SABLE FUNCTIONS ---
 // const bookHover = function () {};
 
-// const bookNotHover = function () {};
+// const bookNotHover = function () {};REU
 
-// when the menu bar is clicked
+// FUNCTIONALITY FOR OPENING THE NAVIGATION BAR
 btnOpenNav.addEventListener("click", function () {
   navigationBar.classList.remove("hidden");
   btnOpenNav.style.visibility = "hidden";
   btnCloseNav.classList.remove("hidden");
 });
 
-// when the close bar is clicked
+// FUNCTIONALITY FOR CLOSING THE NAVIGATION BAR
 btnCloseNav.addEventListener("click", function () {
   navigationBar.classList.add("hidden");
   btnOpenNav.style.visibility = "visible";
   btnCloseNav.classList.add("hidden");
 });
 
-// cart hovering
+// HOVERING OVER THE CART FUNCTIONALITY
 cartEL.addEventListener("mouseenter", function () {
   whiteCartEL.style.visibility = "hidden";
   colorCartEL.style.visibility = "visible";
@@ -150,5 +175,102 @@ bookFour.addEventListener("mouseleave", function () {
 // -- BOOK CATEGORIES --
 // -- CATEGORY ONE --
 bcOne.addEventListener("mouseenter", function () {
-  overlayCTOne.classList.remove("hidden");
+  overlayCtOne.classList.remove("hidden");
+  infoOne.style.visibility = "visible";
+});
+bcOne.addEventListener("mouseleave", function () {
+  overlayCtOne.classList.add("hidden");
+  infoOne.style.visibility = "hidden";
+});
+
+// -- CATEGORY TWO --
+bcTwo.addEventListener("mouseenter", function () {
+  overlayCtTwo.classList.remove("hidden");
+  infoTwo.style.visibility = "visible";
+});
+bcTwo.addEventListener("mouseleave", function () {
+  overlayCtTwo.classList.add("hidden");
+  infoTwo.style.visibility = "hidden";
+});
+
+// -- CATEGORY THREE --
+bcThree.addEventListener("mouseenter", function () {
+  overlayCtThree.classList.remove("hidden");
+  infoThree.style.visibility = "visible";
+});
+bcThree.addEventListener("mouseleave", function () {
+  overlayCtThree.classList.add("hidden");
+  infoThree.style.visibility = "hidden";
+});
+
+// -- CATEGORY FOUR --
+bcFour.addEventListener("mouseenter", function () {
+  overlayCtFour.classList.remove("hidden");
+  infoFour.style.visibility = "visible";
+});
+bcFour.addEventListener("mouseleave", function () {
+  overlayCtFour.classList.add("hidden");
+  infoFour.style.visibility = "hidden";
+});
+
+// -- TESTIMONIAL FUNCTIONALITY
+// ## TESTIMONIAL ONE
+testimonialOne.addEventListener("mouseenter", function () {
+  testimonialOne.classList.add("background-color");
+  testNameOne.classList.add("color");
+});
+testimonialOne.addEventListener("mouseleave", function () {
+  testimonialOne.classList.remove("background-color");
+  testNameOne.classList.remove("color");
+});
+
+// ## TESTIMONIAL TWO
+testimonialTwo.addEventListener("mouseenter", function () {
+  testimonialTwo.classList.add("background-color");
+  testNameTwo.classList.add("color");
+});
+testimonialTwo.addEventListener("mouseleave", function () {
+  testimonialTwo.classList.remove("background-color");
+  testNameTwo.classList.remove("color");
+});
+
+// ## TESTIMONIAL THREE
+testimonialThree.addEventListener("mouseenter", function () {
+  testimonialThree.classList.add("background-color");
+  testNameThree.classList.add("color");
+});
+testimonialThree.addEventListener("mouseleave", function () {
+  testimonialThree.classList.remove("background-color");
+  testNameThree.classList.remove("color");
+});
+
+// -- HANDLES HOVERING FUNCTIONALITY --
+// ## FACEBOOK
+facebookEL.addEventListener("mouseenter", function () {
+  outlineFacebook.style.visibility = "hidden";
+  coloredFacebook.style.visibility = "visible";
+});
+facebookEL.addEventListener("mouseleave", function () {
+  outlineFacebook.style.visibility = "visible";
+  coloredFacebook.style.visibility = "hidden";
+});
+
+// ## INSTAGRAM
+instagramEL.addEventListener("mouseenter", function () {
+  outlineInstagram.style.visibility = "hidden";
+  coloredInstagram.style.visibility = "visible";
+});
+instagramEL.addEventListener("mouseleave", function () {
+  outlineInstagram.style.visibility = "visible";
+  coloredInstagram.style.visibility = "hidden";
+});
+
+// ## TWITTER
+twitterEL.addEventListener("mouseenter", function () {
+  outlineTwitter.style.visibility = "hidden";
+  coloredTwitter.style.visibility = "visible";
+});
+twitterEL.addEventListener("mouseleave", function () {
+  outlineTwitter.style.visibility = "visible";
+  coloredTwitter.style.visibility = "hidden";
 });
